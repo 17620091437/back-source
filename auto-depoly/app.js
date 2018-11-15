d12d6f07fb60c657ee5a999291d513008a1ff97f
crazyball <774028406@qq.com>
2018-11-15 21:27:06 +0800

    test

--- auto-depoly/app.js
+++ auto-depoly/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// 测试自动部署
+// 测试自动部署1
 
 // 自动部署前端代码
 app.post('/front-source/', (req, res) => {
